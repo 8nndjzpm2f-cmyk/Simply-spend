11efb7cc91e915353797f2cc02df5b9829e8739a
8nndjzpm2f-cmyk <allison.n.smith96@gmail.com>
2026-03-04 15:14:58 -0700

    Create tailwind.config.js with color scheme

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1DA1F2',
+        secondary: '#14171A',
+        accent: '#F5A623',
+        neutral: '#F4F4F4',
+        'base-100': '#FFFFFF',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
