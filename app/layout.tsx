4cdb5c43e4a6cfd9bfd2e399ab80858e63daec06
8nndjzpm2f-cmyk <allison.n.smith96@gmail.com>
2026-03-04 15:14:42 -0700

    Create Root Layout for Next.js

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Your App Title',
+  description: 'Your app description',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
