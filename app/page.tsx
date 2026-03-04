ffbe69033605bca16b016fe6ad325863f465aaff
8nndjzpm2f-cmyk <allison.n.smith96@gmail.com>
2026-03-04 15:14:39 -0700

    Create home/login page

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to Simply Spend</h1>
+            <p>Please log in to continue.</p>
+            {/* Add login form or button here */}
+        </div>
+    );
+};
+
+export default HomePage;
